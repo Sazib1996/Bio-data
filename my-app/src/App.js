c5c1008d14ec69675c01af961c9733c240304ff6
Sazib1996 <hmsazib21@gmail.com>
2022-04-12 23:41:10 +0600

    aa

---
+++ my-app/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import BioData from './component/BioData';
+function App() {
+  return (
+    <div className="App">
+      <BioData
+        Name="Md Sazib"
+        Eamil="hmsazib21@gmail.com"
+        Phone="01781471993"
+        skills={["js","react","python"]}
+        Enterests={["web 3", "NTF"]}
+        referencedby="SR Sutu"
+      />
+      <BioData
+        Name="Md Sahin"
+        Eamil="hmsahin@gmail.com"
+        Phone="01568954565"
+        skills={["Html","Css","js","React","python"]}
+        Enterests={["web 3", "NTF", "BLACKLINE"]}
+        referencedby="Hm Sazib"
+      />
+    </div>
+  );
+}
+
+export default App;
